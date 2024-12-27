7d6c764d16b4bbdc21faa08bf89725ea3dafd6ff
eky2429 <eky2429@gmail.com>
2024-12-26 20:28:02 -0600

    Added main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#Madlibs game
+#word game where you can create a story
+#by filling in blanks with random words
+
+adj1 = input("Enter an adjective (description): ")
+noun = input("Enter a noun (person, place, thing): ")
+adj2 = input("Enter an adjective (description): ")
+verb = input("Enter a verb ending in 'ing': ")
+adj3 = input("Enter an adjective (description): ")
+
+print(f"Today I went to a {adj1} zoo")
+print(f"In an exhibit, I saw a {noun}")
+print(f"{noun} was {adj2} and {verb}")
+print(f"I was {adj3}")
